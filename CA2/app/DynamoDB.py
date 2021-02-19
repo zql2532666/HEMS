efb992cafdaa1d3f49b0d288ff25ccf2b6ff9433
Derek <ttz2000@gmail.com>
2021-02-19 18:05:24 +0800

    testing light table data retrieval

--- CA2/app/DynamoDB.py
+++ CA2/app/DynamoDB.py
@@ -1,16 +1,19 @@
 import boto3
 from boto3.dynamodb.conditions import Key,Attr
 
-class DynamoDBPuller:
+class DynamoDBEngine:
     def __init__(self):
         self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         self.dht11_table = self.dynamodb.Table('dht11_data')
         self.light_table = self.dynamodb.Table('light_data')
         self.user_table = self.dynamodb.Table('user_data')
 
+    def retrieve_light_data(self):
+        response = self.light_table()
+        print(response)
 
-db_puller = DynamoDBPuller()
-print(db_puller.dht11_table.creation_date_time)
-print(db_puller.light_table.creation_date_time)
-print(db_puller.user_table.creation_date_time)
+db_engine = DynamoDBEngine()
+print(db_engine.dht11_table.creation_date_time)
+print(db_engine.light_table.creation_date_time)
+print(db_engine.user_table.creation_date_time)
     
