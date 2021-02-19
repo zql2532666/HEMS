962fc056d9370ebac7b7a3a3c0cddf326089c74b
Derek <ttz2000@gmail.com>
2021-02-19 18:07:59 +0800

    testing light table data retrieval

--- CA2/app/DynamoDB.py
+++ CA2/app/DynamoDB.py
@@ -9,7 +9,8 @@ class DynamoDBEngine:
         self.user_table = self.dynamodb.Table('user_data')
 
     def retrieve_light_data(self):
-        response = self.light_table()
+        # response = self.light_table().query()
+        response = self.light_table().scan()
         print(response)
 
 db_engine = DynamoDBEngine()
